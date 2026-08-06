f06345e0d92937900a2560f4115f0e3fa207d4ad
Seyoung Oh <osy01036095318@gmail.com>
2026-08-06 14:00:47 +1200

    BMI Calcurator with invalid situation

--- week1/BMI_calc.py
+++ week1/BMI_calc.py
@@ -2,6 +2,16 @@ def main():
     #input user height and weight
     height, weight = map(float, input("Enter your height(meter) and weight(kg): ").split())
 
+    #if the user input is invalid, print error message
+    if height <= 0 or weight <= 0:
+        print("Enter valid height and weight")
+        return
+
+    # if the user input is in centimeters, convert it to meters
+    elif height > 3:
+        print("Height should be in meters, but... this time I can change it to meters for you😒")
+        height = height / 100
+
     #calculate BMI to percentage round to 2 decimal places
     print("BMI: ", round((weight / height ** 2), 2), "%")
 
